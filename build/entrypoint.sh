5ef2a3cdab167d0f3b092b6340b097cfb60623e3
Claudio Costa <cstcld91@gmail.com>
2023-03-01 16:31:18 -0600

    Enable e2e testing (#22)

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
 
 # Start pulseaudio as system wide daemon; for debugging it helps to start in non-daemon mode
-pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
+pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit --disable-shm=true --log-time=true
 
 # Load audio sink
 pactl load-module module-null-sink sink_name="grab" sink_properties=device.description="monitorOUT"
