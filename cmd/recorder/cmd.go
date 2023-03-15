b231856f0a930dbfde59be9be58379cec680de42
streamer45 <cstcld91@gmail.com>
2023-03-15 15:12:46 -0600

    Improve command output logging

---
+++ cmd/recorder/cmd.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+const (
+	logBufferSize = 1024 * 64 // 64KB
+)
+
+func runCmd(cmd string, args string) (*exec.Cmd, error) {
+	log.Printf("running %s: %q", cmd, args)
+	c := exec.Command(cmd, strings.Split(args, " ")...)
+
+	stdout, err := c.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	stderr, err := c.StderrPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	logOutput := func(out io.ReadCloser, name string) {
+		defer out.Close()
+		buf := make([]byte, logBufferSize)
+		for {
+			n, err := out.Read(buf)
+			if err != nil {
+				log.Printf("%s (%s): error reading: %s", cmd, name, err)
+				return
+			}
+			log.Printf("%s (%s): %s\n", cmd, name, strings.TrimSuffix(string(buf[:n]), "\n"))
+		}
+	}
+
+	go logOutput(stdout, "stdout")
+	go logOutput(stderr, "stderr")
+
+	return c, c.Start()
+}
