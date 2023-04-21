56be1602fac34c4360611eeb0af6d5955dacd0ed
streamer45 <cstcld91@gmail.com>
2023-04-21 17:01:48 -0600

    Allow insecure origin for minikube host

--- cmd/recorder/recorder.go
+++ cmd/recorder/recorder.go
@@ -89,7 +89,7 @@ func (rec *Recorder) runBrowser(recURL string) error {
 	}
 	if devMode := os.Getenv("DEV_MODE"); devMode == "true" {
 		opts = append(opts, chromedp.Flag("unsafely-treat-insecure-origin-as-secure",
-			"http://172.17.0.1:8065,http://host.docker.internal:8065,http://mm-server:8065"))
+			"http://172.17.0.1:8065,http://host.docker.internal:8065,http://mm-server:8065,http://host.minikube.internal:8065"))
 		opts = append(opts, chromedp.NoSandbox)
 		contextOpts = append(contextOpts, chromedp.WithLogf(log.Printf))
 		contextOpts = append(contextOpts, chromedp.WithDebugf(log.Printf))
